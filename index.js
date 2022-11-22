c8d416f211bd144bb152e8992341385fde5b2039
rodnelkes <rodnelkes@gmail.com>
2022-11-22 03:17:14 -0500

    Somehow fix image

--- index.js
+++ index.js
@@ -30,31 +30,32 @@ async function uploadImage(image, tokenID, who = undefined) {
     });
 }
 
-function convertImg(input, tokenID) {
-    new Jimp(1280, 720, (err, image) => {
-        for (let i = 0; i < 720; i++) {
-            for (let j = 0; j < 1280; j++) {
-                var pred = input.predictions[0][i][j];
-                for (let k = 0; k < 3; k++) {
-                    pred[k]++;
-                    pred[k] *= 127.5;
-                    pred[k] = Math.round(pred[k])
-                }
-                image.setPixelColor(Jimp.rgbaToInt(pred[0], pred[1], pred[2], 255), j, i);
+async function convertImg(input, tokenID) {
+    const path = `./images/${tokenID}.png`;
+    var image = new Jimp(1280, 720);
+    for (let i = 0; i < 720; i++) {
+        for (let j = 0; j < 1280; j++) {
+            var pred = input.predictions[0][i][j];
+            for (let k = 0; k < 3; k++) {
+                pred[k]++;
+                pred[k] *= 127.5;
+                pred[k] = Math.round(pred[k])
             }
+            image.setPixelColor(Jimp.rgbaToInt(pred[0], pred[1], pred[2], 255), j, i);
         }
-	image.write(`./images/${tokenID}.png`);
-    });
+    }
+    await image.writeAsync(path);
+    return path;
 }
 
 async function uploadNewPrediction(tokenID) {
-    var image_res = await postPrediction(test_input);
-    convertImg(image_res, tokenID);
-    var output = fs.readFile(`./images/${tokenID}.png`, async (err, image) => {
-        var image = new File([output], "image.png", { type: "image/png" });
-        var cid = await uploadImage(image, tokenID);
-	console.log(cid);
-    });
+    const image_res = await postPrediction(test_input);
+    const file = await convertImg(image_res, tokenID);
+    const output = await fs.readFileSync(file);
+    fs.unlinkSync(file);
+    const image = new File([output], "output.png", { type: "image/png" });
+    const cid = await uploadImage(image, tokenID);
+    console.log(cid);
 }
 
 async function main() {
@@ -63,7 +64,7 @@ async function main() {
     const contract = new ethers.Contract(address, abi, provider);
     
     // Make a prediction and get an image from the output
-    uploadNewPrediction("Test");
+    await uploadNewPrediction("Test");
 
     contract.on("mint", (_tokenId, _who, event) => {
         uploadNewPrediction(_tokenId);
